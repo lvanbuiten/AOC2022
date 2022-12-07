8574b3f49288cf0e0622c6158c8732557c1f0906
Lex van Buiten <L.vanBuiten@sigmax.nl>
2022-12-07 12:50:43 +0100

    Day07

---
+++ day07/part2.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+from __future__ import absolute_import
+
+import argparse
+import os
+import sys
+import string
+import copy
+from typing import List
+from dataclasses import dataclass, field
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from support import support
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+AVAILABLE_DISK_SPACE = 70000000
+NEEDED_DISK_SPACE = 30000000
+
+@dataclass
+class Node:
+    dir: str
+    parent: Node
+    childNodes: List[Node] = field(default_factory=list)
+    files: dict[str, int] = field(default_factory=dict)
+
+def compute(s: str) -> int:
+    root = Node('/', None)
+    current = root
+    lines = s.splitlines()
+    for line in lines[1:]:
+        #print(f'{line}')
+        parts = line.split()
+        if (parts[0] == '$'):
+            #command
+            if (parts[1] == 'cd'):
+                if (parts[2] == '..'):
+                    #up
+                    current = current.parent
+                else:
+                    #down
+                    for child in current.childNodes:
+                        if child.dir == parts[2]:
+                            current = child
+                            break
+                pass
+            elif (parts[1] == 'ls'):
+                pass
+        elif (str.isnumeric(parts[0])):
+            #file
+            current.files[parts[1]] = int(parts[0])
+        elif (parts[0] == 'dir'):
+            #dir
+            current.childNodes.append(Node(parts[1], Node(current.dir, current.parent, current.childNodes, current.files)))
+    
+    #print(f'Tree is {root}')
+
+    folderSizes = []
+    def size(current: Node):
+        cSize = 0
+
+        #print(f'current {current.dir}')
+        for child in current.childNodes:
+            cSize += size(child)
+        
+        for f in current.files.values():
+            #print(f'Filesize is {f}')
+            cSize += f        
+
+        folderSizes.append(cSize)
+        return cSize
+    
+    used_space = size(root)
+    free_space = AVAILABLE_DISK_SPACE - used_space
+
+    folderSizes.sort()
+
+    for f in folderSizes:
+        if (f + free_space) >= NEEDED_DISK_SPACE:
+            return f
+
+    return 0
+
+
+INPUT_S = '''\
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+'''
+EXPECTED = 24933642
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
